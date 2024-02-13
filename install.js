b6f6df4818dd8dc2b97d14319051efb28d8c1f39
cocktailpeanut <cocktailpeanuts@proton.me>
2024-02-13 11:59:17 -0500

    update

---
+++ install.js
@@ -0,0 +1,42 @@
+module.exports = {
+  "cmds": {
+    "nvidia": "pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu118",
+    "amd": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6",
+    "default": "pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu"
+  },
+  "requires": [{
+    "type": "conda",
+    "name": "ffmpeg",
+    "args": "-c conda-forge"
+  }],
+  "run": [{
+    "method": "shell.run",
+    "params": {
+      "message": [
+        "git lfs install",
+        "git clone https://huggingface.co/spaces/cocktailpeanut/stable-cascade app"
+      ]
+    }
+  }, {
+    "method": "shell.run",
+    "params": {
+      "venv": "env",
+      "path": "app",
+      "message": [
+        "{{(gpu === 'nvidia' ? self.cmds.nvidia : ((gpu === 'amd' && platform === 'linux') ? self.cmds.amd : self.cmds.default))}}",
+        "pip install gradio",
+        "pip install -r requirements.txt",
+      ]
+    }
+  }, {
+    "method": "fs.share",
+    "params": {
+      "venv": "app/env"
+    }
+  }, {
+    "method": "notify",
+    "params": {
+      "html": "Click the 'start' tab to get started!"
+    }
+  }]
+}
